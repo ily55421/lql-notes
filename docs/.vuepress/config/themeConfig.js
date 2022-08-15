8bf5b299e3a1fc3eaf742407e2ab9c33bb53ec41
lin <linkq@dreamdt.cn>
2022-08-15 15:52:04 +0800

    update

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -68,6 +68,6 @@ module.exports = {
     // 页脚信息
     createYear: 2022, // 博客创建年份
     copyrightInfo:
-      'Eryajf | <a href="https://github.com/eryajf/lql-notes/blob/main/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      'Eryajf ', // 博客版权信息，支持a标签
   }
 }
